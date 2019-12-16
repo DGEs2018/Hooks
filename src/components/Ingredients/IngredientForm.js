788a724fa936381c54fa8277c14ed4d9a5375ae7
Dawit <dawit@ginetta.net>
2019-12-16 12:19:58 +0100

    Refactor to array destructuring, which is more readable

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo((props) => {
-	const inputState = useState({ title: '', amount: '' }); // state can be an array, object, boolean, number, string anything - main difference with class-based components which only allows objects.
+	const [ inputTitle, setInputTitle ] = useState(''); // state can be an array, object, boolean, number, string anything - main difference with class-based components which only allows objects.
+	const [ inputAmount, setInputAmount ] = useState('');
 	const submitHandler = (event) => {
 		event.preventDefault();
 		// ...
@@ -19,14 +20,8 @@ const IngredientForm = React.memo((props) => {
 						<input
 							type="text"
 							id="title"
-							value={inputState[0].title}
-							onChange={(event) => {
-								const newTitle = event.target.value; // a workaround for the problem caused by the closure
-								inputState[1]((previousInputState) => ({
-									title: newTitle,
-									amount: previousInputState.amount
-								}));
-							}}
+							value={inputTitle}
+							onChange={(event) => setInputTitle(event.target.value)}
 
 							// error will now be caused because of nesting the event is a closure, meaning it's locked inside the callback function
 						/>
@@ -37,14 +32,8 @@ const IngredientForm = React.memo((props) => {
 						<input
 							type="number"
 							id="amount"
-							value={inputState[0].amount}
-							onChange={(event) => {
-								const newAmount = event.target.value;
-								inputState[1]((previousInputState) => ({
-									amount: newAmount,
-									title: previousInputState.title
-								}));
-							}}
+							value={inputAmount.amount}
+							onChange={(event) => setInputAmount(event.target.value)}
 						/>
 					</div>
 					<div className="ingredient-form__actions">
